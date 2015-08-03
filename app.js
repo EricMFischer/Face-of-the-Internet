7bc7f9a11d5600e58493658c169b84e08861c06e
Eric Fischer <ericf712@gmail.com>
2015-08-03 16:56:52 -0700

    Sets up functionality of reddit posts

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', ['$scope', function($scope) {
+app.factory('posts', [function() { // factories & services -> both instances of a Provider. created so we can later inject data anywhere.
+  var o = { // by exporting obj that contains post array, we can later add new objects and methods to our services
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ];
+  };
+  return o;
+}]);
+
+// Injects posts service into MainCtrl
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
   $scope.test = 'Hello world!'; // sets up two-way data binding
-  // $scope -> connects controllers and templates
-  // bind functions or variables to $scope to have them avail. in template
-  $scope.posts = [ // defines a list of post titles
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  // $scope -> connects controllers and templates.
+  // Bind functions or variables to $scope to have them avail. in template
+  $scope.posts = posts.posts; // two-way data binding only works with $scope
   $scope.addPost = function() { // how will user execute this fn? -> index
-    if (!$scope.title || $scope.title = '') {return;}
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    if (!$scope.title || $scope.title === '') {return;}
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
 
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
 
