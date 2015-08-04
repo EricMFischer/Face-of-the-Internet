5276b77d82745fb6c821fbe5dc62be33c8d53bef
Eric Fischer <ericf712@gmail.com>
2015-08-04 14:44:26 -0700

    Adds express routing

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,14 +1,15 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+// above: Registers Posts model with global mongoose object. Anywhere mongoose is imported, our model will now be able to interact with the db.
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-require('./models/Posts');
-// above: Registers Posts model with global mongoose object. Anywhere mongoose is imported, our model will now be able to interact with the db.
-require('./models/Comments');
-mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,56 @@
+var mongoose = require('mongoose'); // import Mongoose
+var Post = mongoose.model('Post'); // handle to Post model
+var Comment = mongoose.model('Comment'); // handle to Comment model
 var express = require('express');
 var router = express.Router();
 
+// req: contains data fields about request made to server
+// res: object used to respond to client
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Define the URL for the route /posts. GET.
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) {return next(err);}
+    res.json(posts); // returns JSON list with all posts (i.e. sends JSON response of retrieved posts back to client)
+  });
+});
+
+// POST.
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body); // using mongoose to create a new post object in memory before saving it to db
+  post.save(function(err, post) {
+    if (err) {return next(err);}
+    res.json(post);
+  });
+});
+
+// when we define a route URL with :post in it, this will be run first
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id); // retrieves post obj from db
+  query.exec(function(err, post) {
+    if (err) {return next(err);}
+    if (!post) {return next(new Error('Cannot find post'));}
+
+    req.post = post; // attaches post obj to the req obj
+    return next(); // routeHandler is called
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post); // post was already attached to req object, so our request handler just has to return the JSON back to the client
+});
+
 module.exports = router;
+
+/*
+GET /posts - return a list of posts and associated metadata
+POST /posts - create a new post
+GET /posts/:id - return an individual post with associated comments
+PUT /posts/:id/upvote - upvote a post, notice we use the post ID in the URL
+POST /posts/:id/comments - add a new comment to a post by ID
+PUT /posts/:id/comments/:id/upvote - upvote a comment
+*/
