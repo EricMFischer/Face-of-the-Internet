c936a18931853ae2b5b5790003a6d4f0cc850b2f
Eric Fischer <ericf712@gmail.com>
2015-08-04 17:45:35 -0700

    Implements persistent post data!

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -8,7 +8,12 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts) {
+          return posts.getAll();
+        }] // anytime home state is entered, we'll query all posts from backend before state finishes loading
+      }
     })
     .state('posts', {
       url: '/posts/{id}', // 'id' -> route parameter made avail. to our controller
@@ -18,7 +23,7 @@ function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('posts', [function() { // factories & services -> both instances of a Provider. created so we can later inject data anywhere.
+app.factory('posts', ['$http', function($http) { // factories & services -> both instances of a Provider. created so we can later inject data anywhere.
   var o = { // by exporting OBJECT that contains post array, we can later add new objects and methods to our services
     posts: [
       {title: 'Hey, I love smoothies more than you think.', upvotes: 5},
@@ -28,6 +33,24 @@ app.factory('posts', [function() { // factories & services -> both instances of 
       {title: 'This site is cooler than Reddit', upvotes: 4}
     ]
   };
+  o.getAll = function() { // retrieves posts from backend
+    return $http.get('/posts').success(function(data) { // using Angular $http service to query our posts route
+      // success cb executes when request returns
+      // route returns a list of posts
+      angular.copy(data, o.posts); // copy that list to client-side posts object // .copy makes UI update properly
+    });
+  };
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data) {
+      o.posts.push(data);
+    });
+  };
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data){
+        post.upvotes += 1;
+      });
+    };
   return o;
 }]);
 
@@ -39,15 +62,19 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 
   $scope.addPost = function() { // how to exec? -> "ng-submit="addPost()"
     if (!$scope.title || $scope.title === '') {return;}
-    $scope.posts.push({
+    // $scope.posts.push({
+    //   title: $scope.title,
+    //   link: $scope.link,
+    //   upvotes: 0,
+    //   comments: [
+    //     {author: 'Billy', body: 'Yeah, youre right!', upvotes: 0},
+    //     {author: 'Bob', body: 'Great idea!', upvotes: 0}
+    //   ]
+    // });
+    posts.create({
       title: $scope.title,
-      link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Billy', body: 'Yeah, youre right!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea!', upvotes: 0}
-      ]
-    });
+      link: $scope.link
+    })
     $scope.title = '';
     $scope.link = '';
   };
@@ -59,21 +86,20 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 }]);
 
 app.controller('PostsCtrl', [
-  '$scope', 
-  '$stateParams', // obj with one key per url parameter
-  'posts',
-  function($scope, $stateParams, posts) {
-    $scope.post = posts.posts[$stateParams.id];
+'$scope', 
+'$stateParams', // obj with one key per url parameter
+'posts',
+function($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
 
-    $scope.addComment = function() {
-      if ($scope.body === '') {return;}
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'Anonymous',
-        upvotes: 0
-      });
-      $scope.body = '';
-    };
-
-  }]);
+  $scope.addComment = function() {
+    if ($scope.body === '') {return;}
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'Anonymous',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+}]);
 
